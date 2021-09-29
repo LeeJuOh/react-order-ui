e55c62856ff9d941c357d48b7d4d935aac1f4480
LeeJuOh <>
2021-09-30 08:29:19 +0900

    Initial init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import React, {useEffect, useState} from "react";
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([
+    {
+      productId: 'uuid-1',
+      productName: 'test-name1',
+      category: 'test-ctg1',
+      price: 1000
+    },
+    {
+      productId: 'uuid-2',
+      productName: 'test-name2',
+      category: 'test-ctg2',
+      price: 2000
+    },
+    {
+      productId: 'uuid-3',
+      productName: 'test-name3',
+      category: 'test-ctg3',
+      price: 3000
+    },
+  ]);
+  const [items, setItems] = useState([]);
+  const handleAddClicked = productId => {
+    const product = products.find(v => v.productId === productId)
+    const found = items.find(v => v.productId === productId);
+    const updatedItems = found ? items.map(
+        v => v.productId === productId ? {...v, count: v.count + 1} : v)
+        : [...items,
+          {...product, count: 1}]
+    setItems(updatedItems);
+    console.log(product, "added!")
+  }
+  const handleOrderSubmit = (order) => {
+    if (items.length === 0) {
+      alert("아이템을 추가해 주세요")
+    } else {
+      axios.post(
+          "http://localhost:8080/api/v1/orders",
+          {
+            email: order.email,
+            address: order.address,
+            postcode: order.postcode,
+            orderItems: items.map(v => ({
+              productId: v.productId,
+              category: v.category,
+              price: v.price,
+              quantity: v.count
+            }))
+          }).then(
+          v => alert("주문이 정상적으로 접수되었습니다."),
+          e => {
+            alert("서버 장애");
+            console.error(e);
+          })
+    }
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/v1/products')
+    .then(v => setProducts(v.data));
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container-fluid">
+        <div className="row justify-content-center m-4">
+          <h1 className="text-center">Grids & Circle</h1>
+        </div>
+        <div className="card">
+          <div className="row">
+            <div
+                className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+              <ProductList products={products} onAddClick={handleAddClicked}/>
+            </div>
+            <div className="col-md-4 summary p-4">
+              <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
